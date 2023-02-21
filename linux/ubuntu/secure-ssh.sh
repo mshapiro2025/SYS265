e6cdccd621f06d2461a97d49021333ebb6726a78
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-02-20 22:52:27 -0500

    Update secure-ssh.sh

--- linux/ubuntu/secure-ssh.sh
+++ linux/ubuntu/secure-ssh.sh
@@ -6,6 +6,8 @@
 
 read -p "Input the username for your new SSH user: " username
 useradd -m $username
+mkdir /home/$username/.ssh
+mkdir /home/$username/.ssh/authorized-keys
 chmod 700 /home/$username/.ssh
 chmod 600 /home/$username/.ssh/authorized_keys
 chown -R $username:$username /home/$username/.ssh
