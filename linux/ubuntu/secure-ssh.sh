c850fc5a977df55718dd563bdd7d5034fe1be7e1
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-02-20 22:17:09 -0500

    Update secure-ssh.sh

--- linux/ubuntu/secure-ssh.sh
+++ linux/ubuntu/secure-ssh.sh
@@ -8,13 +8,15 @@ read -p "Input the username for your new SSH user: " username
 read -p "Enter the password for your new SSH user: " password
 useradd -m $username
 mkdir /home/$username/.ssh
-cp /SYS265/linux/public-keys/id_rsa.pub /home/$username/.ssh/authorized_keys
+mkdir /home/$username/.ssh/authorized-keys
 chmod 700 /home/$username/.ssh
 chmod 600 /home/$username/.ssh/authorized_keys
 chown -R $username:$username /home/$username/.ssh
-if  [[ !  -f "/public-keys/id_rsa.pub" ]]
+if  [[ !  -f "/home/shapiro/SYS265/public-keys/id_rsa.pub" ]]
 then
-  git clone https://github.com/mshapiro2025/SYS265/public-keys
+  git clone https://github.com/mshapiro2025/SYS265/linux/public-keys
+else
+  cp home/shapiro/SYS265/linux/public-keys/id_rsa.pub /home/$username/.ssh/authorized_keys
 fi
 exit
 if grep -Fxq "#PermitRootLogin yes" /etc/ssh/sshd_config
