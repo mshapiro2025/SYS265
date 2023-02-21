c7ce903e8d476fd7b0f0f8daf0a0f53c7add009d
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2023-02-21 18:56:20 -0500

    Update secure-ssh.sh

--- linux/ubuntu/secure-ssh.sh
+++ linux/ubuntu/secure-ssh.sh
@@ -4,8 +4,10 @@
 # adds a public key from the local repo or curled from the remote repo
 # removes roots ability to ssh in
 
+# Asks user for new username
 read -p "Input the username for your new SSH user: " username
 
+# Checks inputted username against existing usernames
 if grep -F $username /etc/passwd
 then
   echo "Sorry, that username is already taken. Try again!"
@@ -15,6 +17,9 @@ else
   useradd -m -s /bin/bash $username
 fi
 
+# Checks whether or not the public key already exists on the local machine
+# If not, pulls the local machine from remote repo, then copies it to the new user's .ssh directory and changes necessary perms
+# If it does, copies the key to the new user's .ssh directory and changes necessary perms
 if  [[ !  -f "/home/shapiro/SYS265/linux/public-keys/id_rsa.pub" ]]
 then
   git pull
@@ -31,6 +36,8 @@ else
   chown -R $username:$username /home/$username/.ssh
 fi
 
+# Checks if the root login setting is default
+# If yes, it uncomments and changes it to "no"
 if grep -Fx "#PermitRootLogin yes" /etc/ssh/sshd_config
 then
   sed -i "s/#PermitRootLogin yes/PermitRootLogin no/I" /etc/ssh/sshd_config
